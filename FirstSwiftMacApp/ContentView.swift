fcc8c61f6a064f1ec7faf711b6b8b03365b5d782
Virej Dasani <dasanivirej@gmail.com>
2021-06-10 18:09:06 +0530

    Initial Commit

---
+++ FirstSwiftMacApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftMacApp
+//
+//  Created by Virej Dasani on 10/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstSwiftMacApp/FirstSwiftMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftMacAppApp.swift
+//  FirstSwiftMacApp
+//
+//  Created by Virej Dasani on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
